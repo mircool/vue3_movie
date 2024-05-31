5020bac62e3216a5ccd4566049e4dbf0a6bda837
mircool <ynyjyz@163.com>
2024-05-31 16:35:16 +0800

    添加电影卡片: 更新电影卡片样式并添加唐人街探案 (2015)

---
+++ src/components/Paging.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="flex items-center justify-center text-gray-500 pb-4 mb-12">
+      <a class=page-link>
+        <button class="w-8 h-8 rounded mx-1 my-1 bg-blue-500 text-white">1</button>
+      </a>
+      <a class=page-link>
+        <button class="w-8 h-8 rounded mx-1 my-1 bg-gray-300">2</button>
+      </a>
+      <a class=page-link>
+        <button class="w-8 h-8 rounded mx-1 my-1 bg-gray-300">...</button>
+      </a>
+      <a class=page-link>
+        <button class="w-8 h-8 rounded mx-1 my-1 bg-gray-300">45</button>
+      </a>
+      <a class=page-link href=javascript:void(0)>
+        <button class="w-8 h-8 rounded mx-1 my-1 bg-gray-300">
+          <svg xmlns=http://www.w3.org/2000/svg class="h-5 w-5 mx-auto" viewBox="0 0 20 20"
+               fill=currentColor>
+            <path fill-rule=evenodd
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule=evenodd></path>
+          </svg>
+        </button>
+      </a>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
