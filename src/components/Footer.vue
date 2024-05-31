700ad63b689ec7313777016b972489b0911796cc
mircool <ynyjyz@163.com>
2024-05-31 16:14:11 +0800

    更新了导航栏、样式和路由，提高了用户体验。

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class="fixed bottom-0 w-full flex justify-center items-center bg-primary-100 py-4">
+      <p class=text-gray-500>© 2023 大熊课堂 All rights reserved.</p>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{vue,html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{vue,html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    100: "#31343D",
+                    200: "#9e9e9e",
+                    300: "#212329",
+                    700: "#101114"
+                },
+                secondary: {
+                    100: "#e2e2d5",
+                    200: "#888883"
+                },
+            },
+        },
+    },
+    plugins: [],
 }
 
